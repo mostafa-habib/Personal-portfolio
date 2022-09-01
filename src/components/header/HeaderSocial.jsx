37d2c17dc2ac315706153832f0212b0f69bca97b
Mostafa Habib <sasahabeb86@gmail.com>
2022-09-01 15:43:21 +0200

    Update LinkedIn account

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -4,7 +4,7 @@ import {FaFacebook , FaGithub , FaLinkedin} from 'react-icons/fa'
 function HeaderSocial() {
   return (
     <div className='header_social'>
-        <a href='https://www.linkedin.com/in/mostafa-habib86/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
+        <a href='https://www.linkedin.com/in/mostafahabibb/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
         <a href='https://github.com/mostafa-habib' target="_blank" rel="noreferrer"><FaGithub /></a>
         <a href='https://www.facebook.com/mostafa.habeb.18/' target="_blank" rel="noreferrer"><FaFacebook /></a>
     </div>
