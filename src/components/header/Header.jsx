2b8ba9e4944603e084fe55c2b76bd8db839b6592
Mostafa Habib <sasahabeb86@gmail.com>
2023-02-19 11:13:25 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,6 @@ function Header() {
     <header id="header">
       <div className='container header_container'>
         <h5>Hello I'm</h5>
-//         <h1>Mostafa Habib</h1>
         <h5 className='text-light'>Frontend Developer</h5>
         <CTA />
         <HeaderSocial />
